4184444ece1b309f75e99cd91f405b56c76c1e54
cuevasn050 <140008979+cuevasn050@users.noreply.github.com>
2025-04-20 23:20:39 -0400

    Modificar host y puerto para Render1

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-datos = []
+
+orden_actual = ""
+respuesta = ""
 
 @app.route("/")
 def home():
     return "<h2>Servidor activo</h2>"
 
-@app.route("/recibir", methods=["POST"])
-def recibir():
-    data = request.json
-    datos.append(data)
-    print("📥 Dato recibido:", data)
-    return jsonify({"estado": "recibido"}), 200
+@app.route("/enviar-orden", methods=["POST"])
+def enviar_orden():
+    global orden_actual
+    orden = request.json.get("orden")
+    orden_actual = orden
+    return jsonify({"estado": "orden recibida"}), 200
 
-@app.route("/ver", methods=["GET"])
-def ver():
-    return jsonify(datos), 200
+@app.route("/obtener-orden", methods=["GET"])
+def obtener_orden():
+    return jsonify({"orden": orden_actual}), 200
 
-import os
+@app.route("/enviar-respuesta", methods=["POST"])
+def recibir_respuesta():
+    global respuesta
+    respuesta = request.json.get("respuesta")
+    return jsonify({"estado": "respuesta recibida"}), 200
+
+@app.route("/ver-respuesta", methods=["GET"])
+def ver_respuesta():
+    return jsonify({"respuesta": respuesta}), 200
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
